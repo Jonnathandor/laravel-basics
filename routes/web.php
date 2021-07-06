3683143de31571fd79b4f0e2fadc3b622a70c9dd
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2021-07-06 15:27:21 -0500

    added basic routing and middleware

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    echo "This is a homepage";
+});
+
+Route::get('about', function () {
+    return view('about');
+})->middleware('check');
+
+Route::get('/contact', [ContactController::class, 'index']);
