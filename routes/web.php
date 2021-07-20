089051694ad9bd9736e8a4b5eb1f50e04d9eeac6
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2021-07-20 16:08:04 -0500

    added category model, migration, and controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\CategoryController;
 use App\Models\User;
 
 /*
@@ -28,6 +29,9 @@ Route::get('about', function () {
 })->middleware('check');
 
 Route::get('/contact', [ContactController::class, 'index']);
+
+// Category Controller
+Route::get('/category/all', [CategoryController::class, 'AllCat'])->name('all.category');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     $users = User::all();
