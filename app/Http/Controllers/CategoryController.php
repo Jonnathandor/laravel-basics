26a8d619af7e826f48d3320d1598f6898bfe290e
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2021-07-22 18:33:49 -0500

    added category form, add category route, controller and validation

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -9,4 +9,15 @@ class CategoryController extends Controller
     public function AllCat(){
         return view('admin.category.index');
     }
+
+    public function AddCat(Request $request){
+        $validatedData = $request->validate([
+            'category_name' => 'required|unique:categories|max:255',
+        ],
+        [
+            'category_name.required' => 'Please enter a category name',
+            'category_name.max' => 'The name is too long (255 chars or more)'
+        ]
+    );
+    }
 }
--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -14,26 +14,53 @@
 
     <div class="container">
         <div class="row">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">SL No</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">Created At</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    
-                    <tr>
-                        <th scope="row"></th>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    
-                </tbody>
-            </table>
+
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">All category</div>
+
+
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">SL No</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Created At</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            
+                            <tr>
+                                <th scope="row"></th>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-header">Add category</div>
+                    <div class="card-body">
+                        <form action="{{ route('store.category') }}" method="POST">
+                        @csrf
+                            <div class="form-group">
+                                <label for="category__name">Category Name</label>
+                                <input type="text" name="category_name" class="form-control" id="category__name" aria-describedby="category__name">
+                                @error('category_name')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+                            <button type="submit" class="btn btn-primary">Add Category</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </x-app-layout>
